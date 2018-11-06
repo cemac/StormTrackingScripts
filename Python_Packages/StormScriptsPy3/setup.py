536813c7a66014c41d2d94645a1c8c9f9858d875
hlburns <H.L.Burns@leeds.ac.uk>
2018-11-06 13:11:46 +0000

    feat: update setup.py   Added more features such as dependancy etc.

--- Python_Packages/StormScriptsPy3/setup.py
+++ Python_Packages/StormScriptsPy3/setup.py
@@ -5,8 +5,18 @@ setup(
     version='2.0.0',
     description=('Python3 Paralell Storm Data Scripts for retrieving data on' +
                  'storms in a set region and plotting various stats'),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Scientific/Engineering',
+      ],
     license='BSD-2',
-    packages=['S_box', 'S_dataminer', 'S_calc-plotter'],
+    packages=find_packages(),
+    install_requires=[
+          '',
+      ], # Pip dependencies
+    dependency_links=['http://github.com/user/repo/tarball/master#egg=package-1.0'] # Iris link
     author='University of Leeds',
     author_email='h.l.burns@leeds.ac.uk',
     keywords=['Storms'],
