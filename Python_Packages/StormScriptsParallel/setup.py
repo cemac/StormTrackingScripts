619a990f7a04dca1f2312c6b4720193b593c5b02
hlburns <H.L.Burns@leeds.ac.uk>
2018-11-06 12:48:49 +0000

    feat: packaging
      setting up the ability to call the whole thing as a module
      e.g.
      import StormScriptsSerial as S
      box = S.storminabox(lats lons size run)

---
+++ Python_Packages/StormScriptsParallel/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='StormScriptsParallel',
+    version='1.0.0',
+    description=('Python2 Paralell Storm Data Scripts for retrieving data on' +
+                 'storms in a set region and plotting various stats'),
+    license='BSD-2',
+    packages=['S_box', 'S_dataminer', 'S_calc-plotter'],
+    author='University of Leeds',
+    author_email='h.l.burns@leeds.ac.uk',
+    keywords=['Storms'],
+    url='https://github.com/cemac/StormTrackingScripts'
+)
---
+++ Python_Packages/StormScriptsSerial/__init__.py
@@ -0,0 +1,3 @@
+from S_box import storminbox
+from S_dataminer import dm_functions
+from S_calc-plotter import stormcalcs
